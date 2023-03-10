f0669d5158f568b5351233ca0b815e10b62ce2ff
waghmaresuyog <suyogw1994@gmail.com>
2023-03-10 18:22:37 +0530

    update in child
    update done on the child branch

---
+++ simple/src/myPakage/UpdateOne.java
@@ -0,0 +1,10 @@
+package myPakage;
+
+public class UpdateOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello this is second updatee");
+	}
+
+}
