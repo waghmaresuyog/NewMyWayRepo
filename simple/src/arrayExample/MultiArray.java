79b3621ab274b2a085a1ac25db4c635758d6e0b9
waghmaresuyog <suyogw1994@gmail.com>
2023-03-15 19:37:23 +0530

    Update Array program  on 15 March

---
+++ simple/src/arrayExample/MultiArray.java
@@ -0,0 +1,20 @@
+package arrayExample;
+
+public class MultiArray {
+
+	public static void main(String[] args) {
+		
+		int a [][]= {{0,0},{5,1},{2,2}};
+		
+		for (int i=0;i<2;i++)
+		{
+			for(int j=0;j<2;j++)
+			{
+			System.out.print(a[i][j]);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
