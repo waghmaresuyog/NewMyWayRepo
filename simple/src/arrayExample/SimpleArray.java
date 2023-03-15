3d0214264a3e4f7ab6f674028b2c17f3f28fc2eb
waghmaresuyog <suyogw1994@gmail.com>
2023-03-15 19:19:33 +0530

    do some update

--- simple/src/arrayExample/SimpleArray.java
+++ simple/src/arrayExample/SimpleArray.java
@@ -14,7 +14,23 @@ public class SimpleArray {
 		{
 			System.out.println("value of array is : "+ ar[i]);
 		}
-
+		System.out.println("--------------------------------");
+		// Other way to use array
+		int a[]= {50,60,70,80,90};
+			for (int j=0; j<a.length;j++)
+			{
+				System.out.println("value of array is : "+ a[j]);
+			}
+			
+			// using for each loop
+			
+			String s[]= {"suyog","prasad","anushree","deepak"};
+			//printing array using for each loop
+			for(String i :s)
+			{
+				System.out.println(i);
+			}
+			
 	}
 
 }
