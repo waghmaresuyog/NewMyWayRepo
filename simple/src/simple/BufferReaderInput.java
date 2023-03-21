0ddb84ebebb27fdd879b3310ed02ec2c80ed1909
waghmaresuyog <suyogw1994@gmail.com>
2023-03-21 19:06:04 +0530

    BufferReaderInput is use
    for instant of Scanner i am using the BufferReaderInput

---
+++ simple/src/simple/BufferReaderInput.java
@@ -0,0 +1,19 @@
+package simple;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BufferReaderInput {
+    public static void main(String[] args) throws IOException {
+        // BufferReaderInput bu = new BufferReaderInput(new InputStreamReader(System.in));
+        BufferedReader bu = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter an number:");
+        int a = Integer.parseInt(bu.readLine());
+        System.out.println("The Number is : " + a);
+        System.out.println("Enter Name:");
+        String input = bu.readLine();
+        System.out.println("Your Name is " + input);
+
+    }
+}
