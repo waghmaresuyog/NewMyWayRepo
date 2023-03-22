1966d7d62d1af8ec76d92c959cc6b26fa9d4e512
waghmaresuyog <suyogw1994@gmail.com>
2023-03-22 15:48:35 +0530

    update

---
+++ simple/src/myPakage/CharEq.java
@@ -0,0 +1,30 @@
+package myPakage;
+
+public class CharEq {
+
+    public static void main(String[] args) {
+
+        String input = "abacbc";
+        char search = 'a'; // Character to search is 'a'.
+        char search1 = 'b'; // Character to search is 'b'.
+        char search2 = 'c'; // Character to search is 'c'.
+        int count = 0;
+        int count1 = 0;
+        int count2 = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == search)
+                count++;
+            else if (input.charAt(i) == search1)
+                count1++;
+            else if (input.charAt(i) == search2)
+                count2++;
+        }
+        if ((count == count1) && (count1 == count2)) {
+            System.out.println("All Characters Have Equal Number of Occurrences");
+        } else {
+            System.out.println("All Characters Have Different Number of Occurrences");
+        }
+
+    }
+
+}
--- simple/src/simple/Hello.java
+++
@@ -1,9 +0,0 @@
-package simple;
-
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello suyog");
-        System.out.println("Hellos suyog");
-    }
-
-}
