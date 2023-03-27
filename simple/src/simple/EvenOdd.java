5259cc2b59f6672b6abc84ae87957a506559b246
waghmaresuyog <suyogw1994@gmail.com>
2023-03-27 13:01:33 +0530

    update format

--- simple/src/simple/EvenOdd.java
+++ simple/src/simple/EvenOdd.java
@@ -2,15 +2,12 @@ package simple;
 
 public class EvenOdd {
 
-	public static void main(String[] args) {
-
-		int num = 15;
-		if (num % 2 == 0) {
-			System.out.println("The given Number is even");
-		} else {
-			System.out.println("The given is odd");
-		}
-
-	}
-
+    public static void main(String[] args) {
+        int num = 15;
+        if (num % 2 == 0) {
+            System.out.println("The given Number is even");
+        } else {
+            System.out.println("The given is odd");
+        }
+    }
 }
