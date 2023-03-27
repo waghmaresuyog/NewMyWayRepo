05bf1fe86eb6317f98a060cee5f743f814168291
waghmaresuyog <suyogw1994@gmail.com>
2023-03-27 11:53:10 +0530

    change variable name

--- simple/src/MediumProblem/TaxCalculator.java
+++ simple/src/MediumProblem/TaxCalculator.java
@@ -1,32 +1,30 @@
 package MediumProblem;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
-public class TacCalculator {
+public class TaxCalculator {
     static float tax;
 
     public static float IncomeTax(float income) {
         if (income <= 500000) {
             System.out.println("You are Tax free");
         } else if (income >= 500000 && income < 700000) {
-            float Taxable_AmountOne = income - 500000;
-            tax = (0.10f) * Taxable_AmountOne;
+            float taxableAmountOne = income - 500000;
+            tax = (0.10f) * taxableAmountOne;
             System.out.println("Your are tax is :" + tax);
         } else if (income > 700000 && income <= 1000000) {
-            float Taxable_AmountTwo = income - 700000;
-            tax = (0.15f) * Taxable_AmountTwo + (0.1F * 200000);
+            float taxableAmountTwo = income - 700000;
+            tax = (0.15f) * taxableAmountTwo + (0.1F * 200000);
             System.out.println("Your are tax is :" + tax);
         } else if (income > 1000000 && income <= 1500000) {
-            float amu2 = income - 1000000;
-            tax = (0.20f) * amu2 + (0.15F * 300000) + (0.1F * 200000);
+            float taxableAmountThree = income - 1000000;
+            tax = (0.20f) * taxableAmountThree + (0.15F * 300000) + (0.1F * 200000);
             System.out.println("Your are tax is :" + tax);
         } else {
-            float amu3 = income - 1000000;
-            tax = (0.30f) * amu3 + (0.2F * 500000) + (0.15F * 300000) + (0.1F * 200000);
+            float taxableAmountFour = income - 1000000;
+            tax = (0.30f) * taxableAmountFour + (0.2F * 500000) + (0.15F * 300000) + (0.1F * 200000);
             System.out.println("Your are tax is :" + tax);
         }
         return tax;
