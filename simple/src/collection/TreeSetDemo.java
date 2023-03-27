e5aa818b90953dd01cb5ae3ce4410085e343a7cf
waghmaresuyog <suyogw1994@gmail.com>
2023-03-27 07:55:45 +0530

    format is update

--- simple/src/collection/TreeSetDemo.java
+++ simple/src/collection/TreeSetDemo.java
@@ -6,27 +6,19 @@ import java.util.TreeSet;
 
 public class TreeSetDemo {
 
-	public static void main(String[] args) {
-		
-		TreeSet ts = new TreeSet(Arrays.asList("Santosh","Mayur","Suyog","Anushree","Rana"));
-//		ts.add("Santosh");
-//		ts.add("Suyog");
-//		ts.add("Mayur");
-//		ts.add("Anushree");
-//		ts.add("Rana");
-		//ts.add(100);
-		//ts.add(200);		//same data type is allow 
-	//	ts.add("Santosh");
-		//ts.add(null);  //null not allow 
-		
-		System.out.println(ts);
-		
-		TreeSet num = new TreeSet<>();
-		num.add(500);
-		num.add(200);
-		num.add(400);
-		num.add(100);
-		System.out.println(num);
-	}
+    public static void main(String[] args) {
+
+        TreeSet ts = new TreeSet(Arrays.asList("Santosh", "Mayur", "Suyog", "Anushree", "Rana", "Suyog"));
+        //same duplicate is not allow
+        //ts.add(null);  //null not allow
+        System.out.println(ts);
+
+        TreeSet num = new TreeSet<>();
+        num.add(500);
+        num.add(200);
+        num.add(400);
+        num.add(100);
+        System.out.println(num);
+    }
 
 }
