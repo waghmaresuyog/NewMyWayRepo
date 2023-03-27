5cca5676fa1bfb9a6abb50840743ee87f57c4b9f
waghmaresuyog <suyogw1994@gmail.com>
2023-03-27 12:56:35 +0530

    update buffer

--- simple/src/simple/BufferReaderInput.java
+++ simple/src/simple/BufferReaderInput.java
@@ -7,17 +7,14 @@ import java.io.InputStreamReader;
 public class BufferReaderInput {
     public static void main(String[] args) throws IOException {
 
-        BufferedReader BufferInput = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader bufferInput = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter an number:");
-        int intNumber = Integer.parseInt(BufferInput.readLine());
+        int intNumber = Integer.parseInt(bufferInput.readLine());
         System.out.println("The Number is : " + intNumber);
         System.out.println("Enter Name:");
-        String StringInput = BufferInput.readLine();
-        System.out.println("Your Name is " + StringInput);
+        String stringInput = bufferInput.readLine();
+        System.out.println("Your Name is " + stringInput);
         var number = 10;
         System.out.println(number);
-
-
     }
-
 }
