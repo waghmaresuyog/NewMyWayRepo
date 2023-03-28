b22bdf67394eecc6bc88f455e64f260a9a6c2ab6
waghmaresuyog <suyogw1994@gmail.com>
2023-03-28 11:28:39 +0530

    multi array update

--- simple/src/arrayExample/MultiArray.java
+++ simple/src/arrayExample/MultiArray.java
@@ -1,17 +1,18 @@
 package arrayExample;
+
 import java.util.Arrays;
+
 public class MultiArray {
     public static void main(String[] args) {
         System.out.println("printing another array");
-        int b[][] = {{10, 20}, {20, 30}, {20, 30}};//this is array is 3 row and 2 colum
-        System.out.println(b[0].length);
-        System.out.println(b.length);
+        int arrayId[][] = {{10, 20}, {20, 30}, {20, 30}};//this is array is 3 row and 2 colum
+        System.out.println(arrayId[0].length);
+        System.out.println(arrayId.length);
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 2; j++) {
-                System.out.print(b[i][j] + " ");
+                System.out.print(arrayId[i][j] + " ");
             }
             System.out.println(" ");
         }
     }
-
 }
