5e30ea5f2eddb94c85ce3730edd68bfa2df5a87b
waghmaresuyog <suyogw1994@gmail.com>
2023-03-28 11:34:57 +0530

    method array update

--- simple/src/arrayExample/MethodArray.java
+++ simple/src/arrayExample/MethodArray.java
@@ -1,17 +1,18 @@
 package arrayExample;
+
 public class MethodArray {
     public static void main(String[] args) {
-        int arr[][] = returnArray();
-        pringArray(arr);
+        int arrayValue[][] = returnArray();
+        pringArray(arrayValue);
     }
-    static int[][] returnArray() {
-        int arr[][] = new int[][]{{5, 5}, {6, 6}};
-        return arr;
+    public static int[][] returnArray() {
+        int arrayValue[][] = new int[][]{{5, 5}, {6, 6}};
+        return arrayValue;
     }
-    static void pringArray(int a[][]) {
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a.length; j++) {
-                System.out.print(a[i][j] + "  ");
+    public static void pringArray(int paraArray[][]) {
+        for (int i = 0; i < paraArray.length; i++) {
+            for (int j = 0; j < paraArray.length; j++) {
+                System.out.print(paraArray[i][j] + "  ");
             }
             System.out.println(" ");
         }
