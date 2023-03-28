fcdb9c522db4dd161c3a7b6778eca1c1f3a7c818
waghmaresuyog <suyogw1994@gmail.com>
2023-03-28 19:10:40 +0530

    update as per comment

--- simple/src/myPakage/CharEq.java
+++ simple/src/myPakage/CharEq.java
@@ -5,21 +5,21 @@ public class CharEq {
     public static void main(String[] args) {
 
         String input = "abacbc";
-        char search = 'a'; // Character to search is 'a'.
-        char search1 = 'b'; // Character to search is 'b'.
-        char search2 = 'c'; // Character to search is 'c'.
-        int count = 0;
-        int count1 = 0;
-        int count2 = 0;
+        char searchOne = 'a'; // Character to search is 'a'.
+        char searchTwo = 'b'; // Character to search is 'b'.
+        char searchThree = 'c'; // Character to search is 'c'.
+        int countOne = 0;
+        int countTwo = 0;
+        int countThree = 0;
         for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) == search)
-                count++;
-            else if (input.charAt(i) == search1)
-                count1++;
-            else if (input.charAt(i) == search2)
-                count2++;
+            if (input.charAt(i) == searchOne)
+                countOne++;
+            else if (input.charAt(i) == searchTwo)
+                countTwo++;
+            else if (input.charAt(i) == searchThree)
+                countThree++;
         }
-        if ((count == count1) && (count1 == count2)) {
+        if ((countOne == countTwo) && (countTwo == countThree)) {
             System.out.println("All Characters Have Equal Number of Occurrences");
         } else {
             System.out.println("All Characters Have Different Number of Occurrences");
