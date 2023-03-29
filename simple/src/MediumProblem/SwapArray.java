5f04b30ecc2d419075580e7442417150fddd9c42
waghmaresuyog <suyogw1994@gmail.com>
2023-03-29 17:35:45 +0530

    swap array is updated as per comment

--- simple/src/MediumProblem/SwapArray.java
+++ simple/src/MediumProblem/SwapArray.java
@@ -5,32 +5,34 @@ import java.util.Arrays;
 public class SwapArray {
     int temp;
     int lastNumber;
-    boolean flag = false;
     public void endSwap(int arrayOne[]) {
         // find last value of array
+        int flag = 0;
         lastNumber = arrayOne[arrayOne.length - 1];
-        for (int i = 2; i <= (lastNumber / 2); i++) {
+        for (int i = 2; i <= (lastNumber -1); i++) {
             if (lastNumber % i == 0) {
-                flag = true;
-                System.out.println("The last Number is :" + lastNumber + "And it is not prime Number");
-                break;
+                flag = flag+1;
             }
         }
         //swap the array if last element is prime
-        if (flag == false) {
+        if (flag >0) {
+            System.out.println("The last Number is :" + lastNumber + "  And it is not prime Number");
+        }
+        else {
             System.out.println("Array last element is : " + lastNumber + "   And Number is prime");
             temp = arrayOne[0];
             arrayOne[0] = lastNumber;
             arrayOne[arrayOne.length - 1] = temp;
             System.out.println("new array is : " + Arrays.toString(arrayOne));
-        } else {
-            System.out.println("Last number of array is :" + lastNumber + "is not prime so not swaped");
         }
     }
     public static void main(String[] args) {
         SwapArray swapArray = new SwapArray();
         int[] arrayOne = new int[]{1, 2, 3, 7};
         swapArray.endSwap(arrayOne);
+        int[] arrayTwo = new int[]{1, 2, 3, 8};
+        swapArray.endSwap(arrayTwo);
+
     }
 }
 
