363c6282609a42ca4245cf7b5d2cdf1d60a4b46c
waghmaresuyog <suyogw1994@gmail.com>
2023-03-29 09:51:24 +0530

    even odd is updated

--- simple/src/simple/EvenOdd.java
+++ simple/src/simple/EvenOdd.java
@@ -2,13 +2,13 @@ package simple;
 
 public class EvenOdd {
     public void even_Odd_Number(int number) {
-        //  int num = 15;
         if (number % 2 == 0) {
             System.out.println("The given Number is even");
         } else {
             System.out.println("The given is odd");
         }
     }
+
     public static void main(String[] args) {
         EvenOdd evenOdd = new EvenOdd();
         evenOdd.even_Odd_Number(15);
