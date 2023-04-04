5542b304fa332d2b4b27aff82ecb5e36fc9f615e
waghmaresuyog <suyogw1994@gmail.com>
2023-04-04 14:49:04 +0530

    update code as per comment use mehtod in program

--- simple/src/simple/CheckAlpha.java
+++ simple/src/simple/CheckAlpha.java
@@ -5,16 +5,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
-
 public class CheckAlpha {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferInput = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter any key on keyboard ");
-        char charInput = (char) bufferInput.read();
+
+    public static void checkAlpha(char charInput)
+    {
         if (Character.isAlphabetic(charInput)) {
             System.out.println(charInput + "  Is an alphabet.");
         } else {
             System.out.println(charInput + "  Is NOT alphabet.");
         }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferInput = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter any key on keyboard ");
+        char charInput = (char) bufferInput.read();
+        checkAlpha(charInput);
     }
 }
