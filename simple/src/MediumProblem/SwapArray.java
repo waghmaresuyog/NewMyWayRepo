92e27bf99e16acfa973b19e16799a9f9ac86ae7e
waghmaresuyog <suyogw1994@gmail.com>
2023-04-07 14:40:20 +0530

    update the swaparray as per comment

--- simple/src/MediumProblem/SwapArray.java
+++ simple/src/MediumProblem/SwapArray.java
@@ -7,12 +7,8 @@ public class SwapArray {
     private int lastNumber;
 
     public void endSwap(int arrayOne[]) {
-        // find last value of array
-        int flag = 0;
         lastNumber = arrayOne[arrayOne.length - 1];
-
         if (lastNumber % 2 == 0) {
-            flag = flag + 1;
             System.out.println("this is not a prime number");
         } else {
             System.out.println("Array last element is : " + lastNumber + "   And Number is prime");
@@ -25,7 +21,7 @@ public class SwapArray {
 
     public static void main(String[] args) {
         SwapArray swapArray = new SwapArray();
-        int[] arrayOne = new int[]{1, 2, 3, 7};
+        int[] arrayOne = new int[]{1, 2, 3, 61};
         swapArray.endSwap(arrayOne);
     }
 }
