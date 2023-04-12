1288c6bd344a7ecd1c9fe117a1249e833511509f
waghmaresuyog <suyogw1994@gmail.com>
2023-04-12 13:32:11 +0530

    change in logic of income tax calculator

--- simple/src/MediumProblem/TaxCalculator.java
+++ simple/src/MediumProblem/TaxCalculator.java
@@ -8,35 +8,26 @@ public class TaxCalculator {
     private float tax;
 
     public float IncomeTax(float income) {
-        int slab = (income < 500000) ? 0 : (500000 <= income && income <= 700000)
-                ? 10 : (700000 <= income && income <= 1000000) ? 15 : (1000000 < income && income >= 1500000) ? 30 : 20;
+        int slab = (income < 700000) ? 1 : (income > 500000 && income <= 1000000)
+                ? 2 : (income <= 1500000) ? 3 : 4;
         switch (slab) {
-            case 0:
-                System.out.println("you are tax free ");
+            case 1:
+                System.out.println(" your income is "+income+"  and you are tax free ");
                 break;
-            case 10:
-                float taxableAmountOne = income - 500000;
-                tax = (0.10f) * taxableAmountOne;
-                System.out.println("Your are tax is :" + tax);
-                System.out.println("you are in 10 %");
-                break;
-            case 15:
+            case 2:
                 float taxableAmountTwo = income - 700000;
                 tax = (0.15f) * taxableAmountTwo + (0.1F * 200000);
-                System.out.println("Your are tax is :" + tax);
-                System.out.println("you are in 15 %");
+                System.out.println( " your income is "+income+ "  Your are tax is 15 % :" + tax);
                 break;
-            case 20:
+            case 3:
                 float taxableAmountThree = income - 1000000;
                 tax = (0.20f) * taxableAmountThree + (0.15F * 300000) + (0.1F * 200000);
-                System.out.println("Your are tax is :" + tax);
-                System.out.println("you are in 20 %");
+                System.out.println(" your income is "+income+ "  Your are tax is 20 % :" + tax);
                 break;
-            case 30:
+            case 4:
                 float taxableAmountFour = income - 1000000;
                 tax = (0.30f) * taxableAmountFour + (0.2F * 500000) + (0.15F * 300000) + (0.1F * 200000);
-                System.out.println("Your are tax is :" + tax);
-                System.out.println("you are in 30 %");
+                System.out.println(" your income is "+income+ "  Your are tax is 30 % :" + tax);
                 break;
             default:
                 System.out.println("Enter valid income");
@@ -46,7 +37,10 @@ public class TaxCalculator {
 
     public static void main(String[] args) {
         TaxCalculator taxCalculator = new TaxCalculator();
+        taxCalculator.IncomeTax(400000);// 0% tax free
         taxCalculator.IncomeTax(600000);
-        taxCalculator.IncomeTax(2200000);
+        taxCalculator.IncomeTax(900000);//15
+        taxCalculator.IncomeTax(1200000);//20%
+        taxCalculator.IncomeTax(1800000);//30 %
     }
 }
