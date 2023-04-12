fbf1e88915f1fbec23d10afaf47aeb04994ed146
waghmaresuyog <suyogw1994@gmail.com>
2023-04-12 12:06:40 +0530

    change in prime No logic

--- simple/src/MediumProblem/PrimeNo.java
+++ simple/src/MediumProblem/PrimeNo.java
@@ -3,22 +3,23 @@ package MediumProblem;
 public class PrimeNo {
 
     public void checkPrime(int number) {
-        int temp = 0;
-        for (int index = 2; index < number; index++) {
-            if (number % index == 0) {
-                temp = temp + 1;
+        int flag = 0;
+        int mid = number / 2;
+        for (int counter = 2; counter <= mid; counter++) {
+            if (number % counter == 0) {
+                flag = 1;
                 break;
             }
         }
-        if (temp == 0) {
-            System.out.println(number + "  Number is Prime");
+        if (flag == 1 || number == 0 || number == 1) {
+            System.out.println("The given number is  : " + number + " is not prime ");
         } else {
-            System.out.println(number + "  Number is not Prime");
+            System.out.println("The given number is  : " + number + " And Number is prime");
         }
     }
 
     public static void main(String[] args) {
         PrimeNo primeNo = new PrimeNo();
-        primeNo.checkPrime(7);
+        primeNo.checkPrime(9);
     }
 }
