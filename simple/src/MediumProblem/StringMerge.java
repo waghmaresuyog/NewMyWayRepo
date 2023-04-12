86cba2139fff030c85ee091b1d2740aef594def8
waghmaresuyog <suyogw1994@gmail.com>
2023-04-12 10:38:41 +0530

    working on comment

--- simple/src/MediumProblem/StringMerge.java
+++ simple/src/MediumProblem/StringMerge.java
@@ -1,25 +1,30 @@
 package MediumProblem;
 
 public class StringMerge {
-    public void merge(String nameOne, String nameTwo) {
+    public String merge(String nameOne, String nameTwo) {
         int lengthNameOne = 0, lengthNameTwo = 0;
+        String string = "";
 
         while (lengthNameOne < nameOne.length() || lengthNameTwo < nameTwo.length()) {
             if (lengthNameOne < nameOne.length()) {
                 System.out.print(nameOne.charAt(lengthNameOne));
+                string += string+nameOne.charAt(lengthNameOne);
                 lengthNameOne++;
             }
             if (lengthNameTwo < nameTwo.charAt(lengthNameTwo)) {
                 System.out.print(nameTwo.charAt(lengthNameTwo));
+                string += string+nameTwo.charAt(lengthNameTwo);
                 lengthNameTwo++;
             }
         }
+        return string;
     }
 
     public static void main(String[] args) {
         StringMerge stringMerge = new StringMerge();
         stringMerge.merge("HI", "there");
         System.out.println(" ");
-        stringMerge.merge("abc", "xyz");
+        String word =  stringMerge.merge("abc", "xyz");
+        System.out.println(" this is from word: "+word);
     }
 }
