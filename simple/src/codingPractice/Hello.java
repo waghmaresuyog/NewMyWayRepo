c2ef86feae93303c1e5457c34cbd54ed5cd32cf6
waghmaresuyog <suyogw1994@gmail.com>
2023-05-19 07:56:04 +0530

    one package is added

---
+++ simple/src/codingPractice/Hello.java
@@ -0,0 +1,7 @@
+package codingPractice;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
--- simple/src/myPakage/UpdateOne.java
+++ simple/src/myPakage/UpdateOne.java
@@ -5,32 +5,6 @@ public class UpdateOne {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("Hello this is second updatee");
-			
-			String input = "abacbc";
-			char search = 'a'; // Character to search is 'a'.
-			char search1 ='b'; // Character to search is 'b'.
-			char search2='c'; // Character to search is 'c'.
-			int count=0;
-			int count1=0;
-			int count2=0;
-			for(int i=0; i<input.length(); i++)
-			{
-			if(input.charAt(i) == search)
-			count++;
-			else if(input.charAt(i) == search1)
-			count1++;
-			else if(input.charAt(i) == search2)
-			count2++;
-			}
-			if ((count==count1) && (count1==count2))
-			{
-			System.out.println("All Characters Have Equal Number of Occurrences");
-			}
-			else
-			{
-			System.out.println("All Characters Have Different Number of Occurrences");
-			}
-			
 	}
 
 }
