6234ff7477565da82acd4cd3f35d13666a84bad6
waghmaresuyog <suyogw1994@gmail.com>
2023-05-19 07:31:13 +0530

    hello updated

--- simple/src/simple/SimpleHello.java
+++ simple/src/simple/SimpleHello.java
@@ -4,7 +4,7 @@ public class SimpleHello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello suyog");
+		System.out.println("Hello ");
 		
 	}
 
