69a8d6d989db0ea3e0f94eb2bbb1958ed71e45d9
waghmaresuyog <suyogw1994@gmail.com>
2023-05-29 11:21:13 +0530

    daily coding practice 6 is added

---
+++ simple/src/codingPractice/ShapeOfTraingle.java
@@ -0,0 +1,9 @@
+package codingPractice;
+
+public class ShapeOfTraingle {
+    public double calculateArea()
+    {
+
+        return 0.0;
+    }
+}
---
+++ simple/src/codingPractice/Triangle.java
@@ -0,0 +1,16 @@
+package codingPractice;
+
+public class Triangle extends ShapeOfTraingle {
+
+    private double base;
+    private double height;
+    public Triangle(double base, double height){
+        this.base=base;
+        this.height=height;
+    }
+
+    @Override
+    public double calculateArea() {
+        return 0.5 * base * height;
+    }
+}
