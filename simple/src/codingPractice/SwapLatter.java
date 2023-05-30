1084243ca5a469e09368d604cfcd87b3e983072c
waghmaresuyog <suyogw1994@gmail.com>
2023-05-30 10:18:22 +0530

    daily coding Practice 7 added

---
+++ simple/src/codingPractice/SwapLatter.java
@@ -0,0 +1,20 @@
+package codingPractice;
+
+public class SwapLatter {
+    public static void main(String[] args) {
+
+        String swapName= "suyog waghmare";
+        System.out.println("the Original string is "+swapName);
+        SwapLatter swapLatter = new SwapLatter();
+
+        System.out.println(swapLatter.swap(swapName,0,7));
+    }
+    public  char [] swap(String swapName,int indexOne, int indexTwo)
+    {   char charSwap[] =swapName.toCharArray();
+        char tempChar = charSwap[indexOne];
+        charSwap[indexOne]=charSwap[indexTwo];
+        charSwap[indexTwo]=tempChar;
+
+        return charSwap;
+    }
+}
