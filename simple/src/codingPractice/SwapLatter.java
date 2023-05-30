114490a58fb59e54204800e091d7454ab97c45fd
waghmaresuyog <suyogw1994@gmail.com>
2023-05-30 13:37:02 +0530

    update done as per comment

--- simple/src/codingPractice/SwapLatter.java
+++ simple/src/codingPractice/SwapLatter.java
@@ -2,19 +2,19 @@ package codingPractice;
 
 public class SwapLatter {
     public static void main(String[] args) {
-
-        String swapName= "suyog waghmare";
-        System.out.println("the Original string is "+swapName);
+        String swapName = "suyog waghmare";
+        System.out.println("the Original string is " + swapName);
         SwapLatter swapLatter = new SwapLatter();
-
-        System.out.println(swapLatter.swap(swapName,0,7));
+        System.out.println(swapLatter.swap(swapName, 0, 7));
+        char newString[] = swapLatter.swap(swapName, 0, 6);
+        System.out.println(newString);
     }
-    public  char [] swap(String swapName,int indexOne, int indexTwo)
-    {   char charSwap[] =swapName.toCharArray();
-        char tempChar = charSwap[indexOne];
-        charSwap[indexOne]=charSwap[indexTwo];
-        charSwap[indexTwo]=tempChar;
 
+    public char[] swap(String swapName, int indexOne, int indexTwo) {
+        char charSwap[] = swapName.toCharArray();
+        char tempChar = charSwap[indexOne];
+        charSwap[indexOne] = charSwap[indexTwo];
+        charSwap[indexTwo] = tempChar;
         return charSwap;
     }
 }
