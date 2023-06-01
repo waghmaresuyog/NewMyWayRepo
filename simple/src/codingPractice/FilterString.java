34aac7aa40eb33d1f5fac1a09265182e96b149f1
waghmaresuyog <suyogw1994@gmail.com>
2023-06-01 15:23:46 +0530

    remove extra space in code

--- simple/src/codingPractice/FilterString.java
+++ simple/src/codingPractice/FilterString.java
@@ -2,7 +2,6 @@ package codingPractice;
 
 public class FilterString {
     public static void main(String[] args) {
-
         MixString mixString = new MixString();
         mixString.separateSrting("$uy0g");
     }
