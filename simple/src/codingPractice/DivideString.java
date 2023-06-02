b6a317c339dcf7a4fd3f0671149de589671e7b10
waghmaresuyog <suyogw1994@gmail.com>
2023-06-02 12:57:25 +0530

    daily coding practice 10  is added

---
+++ simple/src/codingPractice/DivideString.java
@@ -0,0 +1,28 @@
+package codingPractice;
+
+public class DivideString {
+    public static void main(String[] args) {
+
+        String stringComplete = "aaaabbbbcccc";
+        int length = stringComplete.length();
+        int numberOfPart = 3;
+        int temporary = 0, chars = length / numberOfPart;
+        String[] equalStr = new String[numberOfPart];
+        if (length % numberOfPart != 0) {
+            System.out.println("Sorry this string cannot be divided into " + numberOfPart + " equal parts.");
+        } else {
+
+            for (int index = 0; index < length; index = index + chars) {
+                String part = stringComplete.substring(index, index + chars);
+                equalStr[temporary] = part;
+                temporary++;
+
+            }
+            System.out.println(numberOfPart + " equal parts of given string are ");
+
+            for (int index = 0; index < equalStr.length; index++) {
+                System.out.println(equalStr[index]);
+            }
+        }
+    }
+}
