916ab216e3ff331f79602cb3779bc591c06977b2
waghmaresuyog <suyogw1994@gmail.com>
2023-06-07 18:29:02 +0530

    change the logic

--- simple/src/codingPractice/RemoveDuplicateExample.java
+++ simple/src/codingPractice/RemoveDuplicateExample.java
@@ -1,32 +1,17 @@
 package codingPractice;
 
-public class RemoveDuplicateExample {
-    public static int removeDuplicateElements(int arrayNumber[], int length) {
-        if (length == 0 || length == 1) {
-            return length;
-        }
-        int[] buffer = new int[length];
+import java.util.HashSet;
 
-        int temporaryNumber = 0;
-        for (int count = 0; count < length - 1; count++) {
-            if (arrayNumber[count] != arrayNumber[count + 1]) {
-                buffer[temporaryNumber++] = arrayNumber[count];
-            }
-        }
-        buffer[temporaryNumber++] = arrayNumber[length - 1];
-        // Changing original array
-        for (int index = 0; index < temporaryNumber; index++) {
-            arrayNumber[index] = buffer[index];
-        }
-        return temporaryNumber;
-    }
+public class RemoveDuplicateExample {
 
     public static void main(String args[]) {
-        int arrayNumber[] = {10, 20, 20, 30, 30, 40, 50, 50};
-        int length = arrayNumber.length;
-        length = removeDuplicateElements(arrayNumber, length);
-        //printing array elements
-        for (int index = 0; index < length; index++)
-            System.out.print(arrayNumber[index] + " ");
+        int arrayNumber[] = {1, 3, 2, 3, 3, 4, 5, 5};
+        HashSet<Integer> uinqueSet = new HashSet<>();
+        for (int index = 0; index < arrayNumber.length; index++) {
+            uinqueSet.add(arrayNumber[index]);
+        }
+        for (int count : uinqueSet) {
+            System.out.print(count + " ");
+        }
     }
 }
