c7fc5ee36841dfe652c247d88bb4414199d0fdc3
waghmaresuyog <suyogw1994@gmail.com>
2023-06-07 18:36:45 +0530

    change the logic

--- simple/src/codingPractice/RemoveDuplicateExample.java
+++ simple/src/codingPractice/RemoveDuplicateExample.java
@@ -4,14 +4,18 @@ import java.util.HashSet;
 
 public class RemoveDuplicateExample {
 
-    public static void main(String args[]) {
-        int arrayNumber[] = {1, 3, 2, 3, 3, 4, 5, 5};
-        HashSet<Integer> uinqueSet = new HashSet<>();
+    public static void removeDuplicates(int[] arrayNumber) {
+        HashSet<Integer> uniqueSet = new HashSet<>();
         for (int index = 0; index < arrayNumber.length; index++) {
-            uinqueSet.add(arrayNumber[index]);
+            uniqueSet.add(arrayNumber[index]);
         }
-        for (int count : uinqueSet) {
+        for (int count : uniqueSet) {
             System.out.print(count + " ");
         }
+    }
+
+    public static void main(String args[]) {
+        int arrayNumber[] = {1, 3, 2, 3, 3, 4, 5, 5};
+        removeDuplicates(arrayNumber);
     }
 }
