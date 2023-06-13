98e36f97f71696337033a0d8b41f1524672a3911
waghmaresuyog <suyogw1994@gmail.com>
2023-06-13 14:11:31 +0530

    sort the list

--- simple/src/codingPractice/MergeTwoList.java
+++ simple/src/codingPractice/MergeTwoList.java
@@ -1,42 +1,49 @@
 package codingPractice;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class MergeTwoList {
 
-    public static List mergeLists(List numberListOne, List numberListTwo) {
-        List mergeList = new ArrayList<>();
-        int maxLength = Math.max(numberListOne.size(), numberListTwo.size());
+    public static <T> List<T> mergeLists(List<T> listOne, List<T> listTwo) {
+        List<T> mergedList = new ArrayList<>();
+        int maxLength = Math.max(listOne.size(), listTwo.size());
         for (int index = 0; index < maxLength; index++) {
-            if (index < numberListOne.size()) {
-                mergeList.add(numberListOne.get(index));
+            if (index < listOne.size()) {
+                mergedList.add(listOne.get(index));
             }
-            if (index < numberListTwo.size()) {
-                mergeList.add(numberListTwo.get(index));
+            if (index < listTwo.size()) {
+                mergedList.add(listTwo.get(index));
             }
         }
 
-        return mergeList;
+        return mergedList;
     }
-
 
     public static void main(String[] args) {
         List<Integer> numberListOne = List.of(1, 3, 6, 7);
-        List<Integer> numberListTwo = List.of(2, 4, 8, 9);
-        System.out.println("List of Number is : ");
+        List<Integer> numberListTwo = List.of(9, 4, 2, 8);
+        System.out.println("List of Numbers:");
         System.out.println(numberListOne);
         System.out.println(numberListTwo);
-        List mergedNumbers = MergeTwoList.mergeLists(numberListOne, numberListTwo);
-        System.out.println("The Merge Number is : " + mergedNumbers);
+
+        List<Integer> mergedNumbers = mergeLists(numberListOne, numberListTwo);
+        System.out.println("Unsorted Number list is : "+mergedNumbers);
+        Collections.sort(mergedNumbers); // Sort the merged list
+
+        System.out.println("Merged and Sorted Numbers:");
+        System.out.println(mergedNumbers);
+
         List<String> colorOne = List.of("Red", "Green", "Blue");
         List<String> colorTwo = List.of("White", "Black", "Orange", "Pink");
-        System.out.println("List of color is ");
+        System.out.println("List of Colors:");
         System.out.println(colorOne);
         System.out.println(colorTwo);
-        System.out.println("Merged Colors is : ");
-        List mergeCorlor = MergeTwoList.mergeLists(colorOne, colorTwo);
-        System.out.println(mergeCorlor);
+
+        List<String> mergedColors = mergeLists(colorOne, colorTwo);
+        Collections.sort(mergedColors); // Sort the merged list
+
+        System.out.println("Merged and Sorted Colors:");
+        System.out.println(mergedColors);
 
 
     }
