6483d4b3f7a4e78c30c6be07c00c6136f1c48190
waghmaresuyog <suyogw1994@gmail.com>
2023-06-19 10:30:29 +0530

    update the code remove extra space

--- simple/src/codingPractice/ReverseWords.java
+++ simple/src/codingPractice/ReverseWords.java
@@ -1,14 +1,13 @@
 package codingPractice;
 
 public class ReverseWords {
+
     public static String reverseWords(String sentence) {
         String[] words = sentence.trim().split("\\s+");
         StringBuilder reversed = new StringBuilder();
-
         for (int index = words.length - 1; index >= 0; index--) {
             reversed.append(words[index]).append(" ");
         }
-
         return reversed.toString().trim();
     }
 
@@ -19,5 +18,4 @@ public class ReverseWords {
         System.out.println("After Replacing in reverse order ");
         System.out.println(reversedWords);
     }
-
 }
