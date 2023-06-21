0c3a0b3a11a7a3c8c1d450aea6306c52972ba235
waghmaresuyog <suyogw1994@gmail.com>
2023-06-21 12:02:02 +0530

    daily coding practice 21 added

---
+++ simple/src/codingPractice/StreamString.java
@@ -0,0 +1,18 @@
+package codingPractice;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamString {
+    public static void main(String[] args) {
+        List<String> color = Arrays.asList("Red","Green","Blue","Pink","Brown");
+        System.out.println("Original List of String of color "+color);
+        char startingLetter = 'B';
+        long length =color.stream().filter(sort ->sort.startsWith(String.valueOf(startingLetter))).count();
+        System.out.println("\nNumber of colors starting with '" + startingLetter + "': " + length);
+
+        char startingLetterY = 'Y';
+        length =color.stream().filter(sort ->sort.startsWith(String.valueOf(startingLetterY))).count();
+        System.out.println("\nNumber of colors starting with '" + startingLetterY + "': " + length);
+    }
+}
