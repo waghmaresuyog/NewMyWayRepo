22d91fb7f52419460eff0a8de01c5edc8c1388ca
waghmaresuyog <suyogw1994@gmail.com>
2023-07-07 16:32:14 +0530

    change code as per comment

--- simple/src/codingPractice/SpecificWord.java
+++ simple/src/codingPractice/SpecificWord.java
@@ -9,21 +9,24 @@ public class SpecificWord {
     public static void main(String[] args) {
         //create color list
         List<String> colors = Arrays.asList("Red", "Green", "Blue", "Orange", "Black");
-        System.out.println("The colour list is : "+colors.toString());
+        System.out.println("The colour list is : " + colors.toString());
 
         // Specify the word to search for
         String searchColor = "Orange";
-
         // Check if the list contains the specified color using a lambda expression
-        Predicate<String> containWord = word -> word.equals(searchColor);
-        boolean flag = colors.stream().anyMatch(containWord);
-        System.out.println("Is the color " + searchColor + " present in the list? " + flag);
+        System.out.println("Is the color " + searchColor + " present in the list? "
+                + searchColorOne(colors, searchColor));
 
         // Specify the word to search for
         String searchColorTwo = "White";
+        // Print the result
+        System.out.println("\nIs the word " + searchColorTwo + " present in the list? "
+                + searchColorOne(colors, searchColorTwo));
+    }
+
+    public static boolean searchColorOne(List<String> colors, String searchColorTwo) {
         Predicate<String> containWordOne = word -> word.equals(searchColorTwo);
         boolean flagOne = colors.stream().anyMatch(containWordOne);
-        // Print the result
-        System.out.println("\nIs the word " + searchColorTwo + " present in the list? " + flagOne);
+        return flagOne;
     }
 }
