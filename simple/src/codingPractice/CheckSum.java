a3fe8a775cc37169455fa242a730c7777ede40c8
waghmaresuyog <suyogw1994@gmail.com>
2023-07-17 12:27:44 +0530

    Daily coding Practice 28 added

---
+++ simple/src/codingPractice/CheckSum.java
@@ -0,0 +1,25 @@
+package codingPractice;
+
+import java.util.Arrays;
+
+public class CheckSum {
+    public static void main(String[] args) {
+    int [] arrayNumbers={10, 77, 10, 54, -11, 10};
+        System.out.println("Original Array is "+ Arrays.toString(arrayNumbers));
+        int searchNum = 10;
+        int fixedSum = 30;
+        System.out.println("The Result is :"+result(arrayNumbers,searchNum,fixedSum));
+    }
+    public static boolean result(int [] arrayNumbers,int searchNum,int fixedSum){
+        int tempSum=0;
+        for(int number : arrayNumbers ){
+            if(number == searchNum){
+                tempSum +=searchNum;
+            }
+            if(tempSum>fixedSum){
+                break;
+            }
+        }
+        return tempSum==fixedSum;
+    }
+}
