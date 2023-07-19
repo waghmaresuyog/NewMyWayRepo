131eb084c9fc890e07055a7a269c54a22f4a8367
waghmaresuyog <suyogw1994@gmail.com>
2023-07-19 13:10:37 +0530

    update code as per comment

--- simple/src/codingPractice/CheckSum.java
+++ simple/src/codingPractice/CheckSum.java
@@ -4,22 +4,24 @@ import java.util.Arrays;
 
 public class CheckSum {
     public static void main(String[] args) {
-    int [] arrayNumbers={10, 77, 10, 54, -11, 10};
-        System.out.println("Original Array is "+ Arrays.toString(arrayNumbers));
+        int[] arrayNumbers = {10, 77, 10, 54, -11, 10};
+        System.out.println("Original Array is " + Arrays.toString(arrayNumbers));
         int searchNum = 10;
         int fixedSum = 30;
-        System.out.println("The Result is :"+result(arrayNumbers,searchNum,fixedSum));
+        System.out.println("The Result is :" + result(arrayNumbers, searchNum, fixedSum));
     }
-    public static boolean result(int [] arrayNumbers,int searchNum,int fixedSum){
-        int tempSum=0;
-        for(int number : arrayNumbers ){
-            if(number == searchNum){
-                tempSum +=searchNum;
+
+    public static boolean result(int[] arrayNumbers, int searchNum, int fixedSum) {
+
+        int tempSum = 0;
+        for (int number : arrayNumbers) {
+            if (number == searchNum) {
+                tempSum += searchNum;
             }
-            if(tempSum>fixedSum){
+            if (tempSum > fixedSum) {
                 break;
             }
         }
-        return tempSum==fixedSum;
+        return tempSum == fixedSum;
     }
 }
